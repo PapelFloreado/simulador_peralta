1f776b3749935e6abab4649065c1fb0970168776
Gabriel Peralta <pgabrieloscar@gmail.com>
2022-05-25 21:04:11 -0300

    entrega for v2

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 // Primer desafio opcional entregable.
-debugger
-let numero = parseInt(prompt("Hola, ingrese un numero del 1 al 10"));
 
-if (numero < 10) {
-    for (let i = 1; i >= 10; i++) {
-        let resultado = numero + i;
-        console.log(resultado);
+let numero = parseInt(prompt("Hola, ingrese un numero del 1 al 20"));
+
+
+for (let i = 1; i <= 20; i++){
+    let resultado = numero + i;
+    console.log("Este es nuestro resultado:",resultado)
+    if (resultado === 10){
+        console.log("Llegaste a la mitad, tranqui...")
+    }else if(resultado === 15){
+        console.warn("Te estas acercando al limite...")
+    }else if(resultado === 20){
+        console.error("Chocaste!!!")
+        break;
+    }else if(numero >= 20){
+        alert("recarga la página... dijimos hasta el 20")
+        break;
     }
-}else{ (numero > 10)
-    alert("menos que 10! volve a empezar!");  
 }
+
+
+
